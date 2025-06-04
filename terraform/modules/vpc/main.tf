684d2d1371a9dbc6188c02741021336050de2019
yotambenz1 <yotam@projectcircle.co.il>
2025-06-04 19:06:24 +0300

    feat: add VPC module for for networking setup

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,27 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.availability_zones
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_flow_log                      = true
+  create_flow_log_cloudwatch_log_group = true
+  create_flow_log_cloudwatch_iam_role  = true
+  flow_log_cloudwatch_log_group_retention_in_days = 30
+
+  tags = merge(
+    var.tags,
+    {
+      Environment = var.environment
+      Terraform   = "true"
+    }
+  )
+} 
